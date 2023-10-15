9dc0fccea8226ab86508f551f10c34ae49b6adb7
lifehappy <858953471@qq.com>
2023-10-15 14:32:32 +0800

    feat: 16 adapter

---
+++ 03Structural-Pattern/16Adapter/src/include/adapterHandle.h
@@ -0,0 +1,21 @@
+#ifndef ADAPTER_H_
+#define ADAPTER_H_
+
+#include <cmath>
+
+#include "server.h"
+#include "clientInterface.h"
+
+// 方钉适配器: 该适配器能让客户端将方钉放入圆孔中
+class SquarePegAdapter : public RoundPeg {
+ public:
+  explicit SquarePegAdapter(SquarePeg* sp) : square_peg_(sp) {}
+  int get_radius() override {
+    return square_peg_->get_width() * sqrt(2) / 2;
+  }
+
+ private:
+  SquarePeg* square_peg_;
+};
+
+#endif  // ADAPTER_H_
